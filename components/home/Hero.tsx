b0fd5cbbdc1e674d6598d3806d3a820f5d479c47
Waleed0260 <muhammadwaleedahsan43@gmail.com>
2025-07-15 21:43:50 +0500

    add

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import Image from 'next/image';
-import HeroImg from "@/public/images/HeroImg.jpeg";
 import { Button } from '../ui/button';
 import Link from 'next/link';
+import HeroImg from "@/public/images/HeroImg.jpeg";
 
 const Hero = () => {
   return (
-    <div className="w-full h-[600px] relative flex items-center justify-center text-white">
-      {/* Background Image */}
-      <Image
-        src={HeroImg}
-        alt="Hero"
-        fill
-        className="object-cover"
-        priority
-      />
+    <div
+      className="w-full h-[20rem] md:h-[30rem] bg-no-repeat bg-center bg-cover flex items-center justify-center text-white"
+      style={{
+        backgroundImage: `url(/images/HeroImg.jpeg)`,
+      }}
+    >
+      {/* Optional dark overlay for better contrast */}
+      {/* <div className="absolute inset-0 bg-black/40 z-0" /> */}
 
-      {/* Overlay */}
-      {/* <div className="absolute inset-0 bg-black bg-opacity-50" /> */}
-
-      {/* Centered Text */}
-      <div className="absolute text-center z-10">
-        <h1 className="md:text-6xl text-4xl font-bold mb-4 head ">Stationery to create <br /> Toys to celebrate</h1>
-        {/* <p className="text-lg">We provide amazing solutions for your business</p> */}
+      <div className="text-center z-10 px-4">
+        <h1 className="md:text-6xl text-4xl font-bold mb-4">
+          Stationery to create <br /> Toys to celebrate
+        </h1>
         <Link href={"/products"}>
-        <Button className='cursor-pointer py-5 px-6'>View Products</Button>
+          <Button className='cursor-pointer py-5 px-6'>View Products</Button>
         </Link>
       </div>
     </div>
